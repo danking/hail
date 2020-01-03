2fd7769ffd049a6febbe7326a02500ea21743e62
Daniel King <dking@broadinstitute.org>
2020-01-03 13:37:51 -0500

    wip

---
+++ gradle-build-cache/start-nginx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+base_path=$(python3 -c 'from hailtop.config import get_deploy_config; print(get_deploy_config().base_path(\"shuffler.shuffler-0\"))')
+
+sed -e "s,@base_path@,${base_path:-/},g" \
+    < /nginx.conf.in > /etc/nginx/conf.d/gradle-build-cache.conf
