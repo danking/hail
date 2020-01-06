b56c6832f10670bde3bde7694195f3e2e55f9651
Daniel King <dking@broadinstitute.org>
2020-01-06 15:59:36 -0500

    updates

--- gradle-build-cache/start-nginx.sh
+++ gradle-build-cache/start-nginx.sh
@@ -2,7 +2,7 @@
 
 base_path=$(python3 -c 'from hailtop.config import get_deploy_config; print(get_deploy_config().base_path("gradle-build-cache"))')
 
-sed -e "s,@base_path@,${base_path:-/},g" \
+sed -e "s,@base_path@,${base_path},g" \
     < /nginx.conf.in > /etc/nginx/conf.d/gradle-build-cache.conf
 
 nginx -g "daemon off;"
