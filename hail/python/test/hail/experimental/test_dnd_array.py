d044f9ff27af33207495caf9f147d190758d4398
Daniel King <dking@broadinstitute.org>
2020-08-27 15:51:45 -0400

    [hail] implement sort_columns for dndarray
    In `main`, dnd array is just very broken if the columns are not ordered by their
    column key.

--- hail/python/test/hail/experimental/test_dnd_array.py
+++ hail/python/test/hail/experimental/test_dnd_array.py
@@ -157,3 +157,42 @@ def test_king_homo_estimator():
                   [4., 6., 0., 6., 0.],
                   [2., 4., 6., 0., 6.],
                   [4., 6., 0., 6., 0.]]))
+
+
+def test_dndarray_errors_on_unsorted_columns():
+    n_variants = 10
+    n_samples = 10
+    block_size = 3
+    mt = hl.utils.range_matrix_table(n_variants, n_samples)
+    mt = mt.key_cols_by(sampleid=hl.str('zyxwvutsrq')[mt.col_idx])
+    mt = mt.select_entries(x=mt.row_idx * mt.col_idx)
+    try:
+        hl.experimental.dnd.array(mt, 'x', block_size=block_size)
+    except ValueError as err:
+        assert 'columns are not in sorted order', err.args[0]
+    else:
+        assert False
+
+
+def test_dndarray_sort_columns():
+    n_variants = 10
+    n_samples = 10
+    block_size = 3
+    disorder = [0, 9, 8, 7, 1, 2, 3, 4, 6, 5]
+    order = [x[0]
+             for x in sorted(enumerate(disorder),
+                             key=lambda x: x[1])]
+    mt = hl.utils.range_matrix_table(n_variants, n_samples)
+    mt = mt.key_cols_by(sampleid=hl.literal(disorder)[mt.col_idx])
+    mt = mt.select_entries(x=mt.row_idx * mt.col_idx)
+    da = hl.experimental.dnd.array(mt, 'x', block_size=block_size, sort_columns=True)
+
+    a = np.array(
+        [r * order[c] for r in range(n_variants) for c in range(n_samples)]
+    ).reshape((n_variants, n_samples))
+
+    assert np.array_equal(da.collect(), a)
+
+    result = (da.T @ da).collect()
+    expected = a.T @ a
+    assert np.array_equal(result, expected)
