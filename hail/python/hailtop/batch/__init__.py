15d827c2a60603fdc2201f158e621523c9ee97c4
Daniel King <dking@broadinstitute.org>
2020-09-28 14:29:28 -0400

    [batch] expose ABC Resource to users
    CHANGELOG: The `Resource` class is now also available at hailtop.batch.Resource.

--- hail/python/hailtop/batch/__init__.py
+++ hail/python/hailtop/batch/__init__.py
@@ -4,6 +4,7 @@ from .batch import Batch
 from .batch_pool_executor import BatchPoolExecutor
 from .backend import LocalBackend, ServiceBackend, Backend
 from .utils import BatchException
+from .resource import Resource
 
 __all__ = ['Batch',
            'LocalBackend',
@@ -11,7 +12,8 @@ __all__ = ['Batch',
            'Backend',
            'BatchException',
            'BatchPoolExecutor',
-           'genetics'
+           'genetics',
+           'Resource'
            ]
 
 nest_asyncio.apply()
