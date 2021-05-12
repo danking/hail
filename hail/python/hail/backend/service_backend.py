903a062421e2f43be12b4471f5bffa3eda528636
Daniel King <dking@broadinstitute.org>
2021-05-12 18:01:59 -0400

    overeager awaits

--- hail/python/hail/backend/service_backend.py
+++ hail/python/hail/backend/service_backend.py
@@ -248,7 +248,7 @@ class ServiceBackend(Backend):
             b = bb.submit(disable_progress_bar=self.disable_progress_bar)
             status = b.wait(disable_progress_bar=self.disable_progress_bar)
             if status['n_succeeded'] != 1:
-                raise ValueError(f'batch failed {status} {await j.log()}')
+                raise ValueError(f'batch failed {status} {j.log()}')
 
 
             with self.fs.open(dir + '/out', 'rb') as outfile:
@@ -287,7 +287,7 @@ class ServiceBackend(Backend):
             b = bb.submit(disable_progress_bar=self.disable_progress_bar)
             status = b.wait(disable_progress_bar=self.disable_progress_bar)
             if status['n_succeeded'] != 1:
-                raise ValueError(f'batch failed {status} {await j.log()}')
+                raise ValueError(f'batch failed {status} {j.log()}')
 
 
             with self.fs.open(dir + '/out', 'rb') as outfile:
@@ -326,7 +326,7 @@ class ServiceBackend(Backend):
             b = bb.submit(disable_progress_bar=self.disable_progress_bar)
             status = b.wait(disable_progress_bar=self.disable_progress_bar)
             if status['n_succeeded'] != 1:
-                raise ValueError(f'batch failed {status} {await j.log()}')
+                raise ValueError(f'batch failed {status} {j.log()}')
 
 
             with self.fs.open(dir + '/out', 'rb') as outfile:
@@ -365,7 +365,7 @@ class ServiceBackend(Backend):
             b = bb.submit(disable_progress_bar=self.disable_progress_bar)
             status = b.wait(disable_progress_bar=self.disable_progress_bar)
             if status['n_succeeded'] != 1:
-                raise ValueError(f'batch failed {status} {await j.log()}')
+                raise ValueError(f'batch failed {status} {j.log()}')
 
 
             with self.fs.open(dir + '/out', 'rb') as outfile:
@@ -463,7 +463,7 @@ class ServiceBackend(Backend):
             b = bb.submit(disable_progress_bar=self.disable_progress_bar)
             status = b.wait(disable_progress_bar=self.disable_progress_bar)
             if status['n_succeeded'] != 1:
-                raise ValueError(f'batch failed {status} {await j.log()}')
+                raise ValueError(f'batch failed {status} {j.log()}')
 
 
             with self.fs.open(dir + '/out', 'rb') as outfile:
