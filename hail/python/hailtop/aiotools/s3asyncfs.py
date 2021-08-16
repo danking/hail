bdca6d4abc716a79f40b3dd67aac020ffea95a26
Daniel King <dking@broadinstitute.org>
2021-08-16 18:37:52 -0400

    fix

--- hail/python/hailtop/aiotools/s3asyncfs.py
+++ hail/python/hailtop/aiotools/s3asyncfs.py
@@ -451,4 +451,5 @@ class S3AsyncFS(AsyncFS):
     def _copy_part_size():
         # Because the S3 upload_part API call requires the entire part
         # be loaded into memory, use a smaller part size.
-        return 8 * 1024 * 1024
+        # return 8 * 1024 * 1024
+        return 32 * 1024 * 1024
