edaa7caccc4c4fb9dd8fdcf79fc6f39f600cf020
Daniel King <dking@broadinstitute.org>
2021-08-16 20:31:47 -0400

    fix

--- hail/python/hailtop/aiotools/fs.py
+++ hail/python/hailtop/aiotools/fs.py
@@ -847,7 +847,7 @@ class Copier:
         # This is essentially a limit on amount of memory in temporary
         # buffers during copying.  We allow ~10 full-sized copies to
         # run concurrently.
-        self.xfer_sema = WeightedSemaphore(20 * Copier.BUFFER_SIZE)
+        self.xfer_sema = WeightedSemaphore(1000 * Copier.BUFFER_SIZE)
 
     async def _dest_type(self, transfer: Transfer):
         '''Return the (real or assumed) type of `dest`.
