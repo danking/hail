112e886b78092cf4e3e4d4785363a7e5e9729b41
Daniel King <dking@broadinstitute.org>
2022-03-19 19:56:39 -0400

    tests passing

--- hail/python/test/hail/methods/test_statgen.py
+++ hail/python/test/hail/methods/test_statgen.py
@@ -455,7 +455,6 @@ class Tests(unittest.TestCase):
 
     logreg_functions = [hl.logistic_regression_rows, hl._logistic_regression_rows_nd] if backend_name == "spark" else [hl._logistic_regression_rows_nd]
 
-    @fails_service_backend(reason='not implemented error: register_ir_function')
     def test_weighted_linear_regression(self):
         covariates = hl.import_table(resource('regressionLinear.cov'),
                                      key='Sample',
--- hail/python/test/hail/vds/test_vds.py
+++ hail/python/test/hail/vds/test_vds.py
@@ -92,7 +92,6 @@ def test_conversion_equivalence():
     assert svcr._same(svcr_readback)
 
 
-@fails_service_backend(reason='register_ir_function')
 def test_sampleqc_old_new_equivalence():
     vds = hl.vds.read_vds(os.path.join(resource('vds'), '1kg_chr22_5_samples.vds'))
     sqc = hl.vds.sample_qc(vds)
