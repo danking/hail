bac8d2598aff826033a0746cebcadce407e7d4ff
Dan King <dking@broadinstitute.org>
2023-09-12 16:29:35 -0400

    remove render noise

--- hail/src/main/scala/is/hail/expr/ir/Optimize.scala
+++ hail/src/main/scala/is/hail/expr/ir/Optimize.scala
@@ -17,9 +17,9 @@ object Optimize {
     }
 
     def runOpt(f: BaseIR => BaseIR, iter: Int, optContext: String): Unit = {
-      render(context + ": before " + optContext)
+      // render(context + ": before " + optContext)
       ir = ctx.timer.time(optContext)(f(ir).asInstanceOf[T])
-      render(context + ": after " + optContext)
+      // render(context + ": after " + optContext)
     }
 
     ctx.timer.time("Optimize") {
