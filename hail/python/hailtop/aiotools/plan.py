45ea05a0c171d465309fb8311fdd607e9dab2da2
Dan King <dking@broadinstitute.org>
2023-10-16 00:29:52 -0400

    plan faster and with debugging info

--- hail/python/hailtop/aiotools/plan.py
+++ hail/python/hailtop/aiotools/plan.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple, Dict, Awaitable
+import aiomonitor
 import argparse
 import asyncio
 import logging
@@ -8,6 +9,7 @@ from io import TextIOWrapper
 
 from .router_fs import RouterAsyncFS
 from .fs import FileListEntry, AsyncFS
+from ..utils.rich_progress_bar import RichProgressBar, Progress
 
 try:
     import uvloop
@@ -43,13 +45,12 @@ async def main() -> None:
         logging.root.setLevel(logging.INFO)
 
     requester_pays_project = args.requester_pays_project
-    gcs_kwargs = {'project': requester_pays_project}
+    gcs_kwargs = {'gcs_requester_pays_configuration': requester_pays_project}
 
     total_n_files = 0
     total_n_bytes = 0
 
-    async with RouterAsyncFS(default_scheme='file',
-                             # local_kwargs=local_kwargs,
+    async with RouterAsyncFS(# local_kwargs=local_kwargs,
                              gcs_kwargs=gcs_kwargs,
                              # azure_kwargs=azure_kwargs,
                              # s3_kwargs=s3_kwargs
@@ -59,18 +60,22 @@ async def main() -> None:
              open('srconly', 'w') as srconly, \
              open('dstonly', 'w') as dstonly, \
              open('plan', 'w') as plan:
-            for src, dst in args.copy:
-                n_files, n_bytes = await find_all_copy_pairs(
-                    fs,
-                    matches,
-                    differs,
-                    srconly,
-                    dstonly,
-                    plan,
-                    src,
-                    dst)
-                total_n_files += n_files
-                total_n_bytes += n_bytes
+            with RichProgressBar(transient=True, disable=not args.verbose) as progress:
+                for src, dst in args.copy:
+                    n_files, n_bytes = await find_all_copy_pairs(
+                        fs,
+                        matches,
+                        differs,
+                        srconly,
+                        dstonly,
+                        plan,
+                        src,
+                        dst,
+                        progress,
+                        asyncio.Semaphore(4)
+                    )
+                    total_n_files += n_files
+                    total_n_bytes += n_bytes
     with open('summary', 'w') as summary:
         summary.write(f'{total_n_files}\t{total_n_bytes}\n')
 
@@ -98,114 +103,133 @@ async def matching_files(fs: AsyncFS, x: str) -> List[Tuple[str, str, bool, int]
 
 
 async def find_all_copy_pairs(
-        fs: AsyncFS,
-        matches: TextIOWrapper,
-        differs: TextIOWrapper,
-        srconly: TextIOWrapper,
-        dstonly: TextIOWrapper,
-        plan: TextIOWrapper,
-        src: str,
-        dst: str
-):
-    srcfiles, dstfiles = await asyncio.gather(matching_files(fs, src), matching_files(fs, dst))
-    srcfiles.sort(key=lambda x: x[0])
-    dstfiles.sort(key=lambda x: x[0])
+    fs: AsyncFS,
+    matches: TextIOWrapper,
+    differs: TextIOWrapper,
+    srconly: TextIOWrapper,
+    dstonly: TextIOWrapper,
+    plan: TextIOWrapper,
+    src: str,
+    dst: str,
+    progress: Progress,
+    sema: asyncio.Semaphore,
+) -> Tuple[int, int]:
+    async with sema:
+        srcfiles, dstfiles = await asyncio.gather(matching_files(fs, src), matching_files(fs, dst))
+        srcfiles.sort(key=lambda x: x[0])
+        dstfiles.sort(key=lambda x: x[0])
 
-    srcidx = 0
-    dstidx = 0
+        tid = progress.add_task(description=src, total=len(srcfiles) + len(dstfiles))
 
-    n_files = 0
-    n_bytes = 0
+        srcidx = 0
+        dstidx = 0
 
-    if len(srcfiles) > 100 or len(dstfiles) > 100:
-        informed = True
-        print(src)
-        sys.stdout.flush()
-        sys.stderr.flush()
-    else:
-        informed = False
+        n_files = 0
+        n_bytes = 0
 
-    while srcidx < len(srcfiles) and dstidx < len(dstfiles):
-        srcf = srcfiles[srcidx]
-        dstf = dstfiles[dstidx]
-        print(f' {srcf}\n {dstf}\n')
-        srcname, srcurl, srcisdir, srcsize = srcf
-        dstname, dsturl, dstisdir, dstsize = dstf
-        if srcname == dstname:
-            if srcisdir and dstisdir:
-                if not informed:
-                    informed = True
-                    print(src)
-                dir_n_files, dir_n_bytes = await find_all_copy_pairs(
-                    fs, matches, differs, srconly, dstonly, plan,
-                    srcurl, dsturl
-                )
-                n_files += dir_n_files
-                n_bytes += dir_n_bytes
-            elif srcisdir and not dstisdir:
-                differs.write(srcurl + '\t' + dsturl + '\t' + 'dir' + '\t' + 'file' + '\n')
-            elif not srcisdir and dstisdir:
-                differs.write(srcurl + '\t' + dsturl + '\t' + 'file' + '\t' + 'dir' + '\n')
-            elif srcsize == dstsize:
-                matches.write(srcurl + '\t' + dsturl + '\n')
+        # if len(srcfiles) > 100 or len(dstfiles) > 100:
+        #     informed = True
+        #     print(src)
+        #     sys.stdout.flush()
+        #     sys.stderr.flush()
+        # else:
+        #     informed = False
+
+        child_directory_tasks: List[asyncio.Task] = []
+        while srcidx < len(srcfiles) and dstidx < len(dstfiles):
+            srcf = srcfiles[srcidx]
+            dstf = dstfiles[dstidx]
+            # print(f' {srcf}\n {dstf}\n')
+            srcname, srcurl, srcisdir, srcsize = srcf
+            dstname, dsturl, dstisdir, dstsize = dstf
+            if srcname == dstname:
+                if srcisdir and dstisdir:
+                    # if not informed:
+                    #     informed = True
+                    #     print(src)
+                    child_directory_tasks.append(
+                        asyncio.create_task(find_all_copy_pairs(
+                            fs, matches, differs, srconly, dstonly, plan, srcurl, dsturl, progress, sema
+                        ))
+                    )
+                elif srcisdir and not dstisdir:
+                    differs.write(srcurl + '\t' + dsturl + '\t' + 'dir' + '\t' + 'file' + '\n')
+                elif not srcisdir and dstisdir:
+                    differs.write(srcurl + '\t' + dsturl + '\t' + 'file' + '\t' + 'dir' + '\n')
+                elif srcsize == dstsize:
+                    matches.write(srcurl + '\t' + dsturl + '\n')
+                else:
+                    differs.write(srcurl + '\t' + dsturl + '\t' + str(srcsize) + '\t' + str(dstsize) + '\n')
+                dstidx += 1
+                srcidx += 1
+                progress.update(tid, advance=2)
+            elif srcname < dstname:
+                if srcisdir:
+                    if src[-1] != '/':
+                        src += '/'
+                    # if not informed:
+                    #     informed = True
+                    #     print(src)
+                    child_directory_tasks.append(
+                        asyncio.create_task(find_all_copy_pairs(
+                            fs, matches, differs, srconly, dstonly, plan, srcurl, os.path.join(dst, removeprefix(srcurl, src)), progress, sema
+                        ))
+                    )
+                else:
+                    srconly.write(srcurl + '\n')
+                    plan.write(srcurl + '\t' + os.path.join(dst, srcname) + '\n')
+                    n_files += 1
+                    n_bytes += srcsize
+                srcidx += 1
+                progress.update(tid, advance=1)
             else:
-                differs.write(srcurl + '\t' + dsturl + '\t' + str(srcsize) + '\t' + str(dstsize) + '\n')
-            dstidx += 1
-            srcidx += 1
-        elif srcname < dstname:
-            srcidx += 1
+                assert srcname >= dstname
+                dstidx += 1
+                progress.update(tid, advance=1)
+                dstonly.write(dsturl + '\n')
+        while srcidx < len(srcfiles):
+            srcf = srcfiles[srcidx]
+            srcname, srcurl, srcisdir, srcsize = srcf
+
             if srcisdir:
                 if src[-1] != '/':
                     src += '/'
-                if not informed:
-                    informed = True
-                    print(src)
-                dir_n_files, dir_n_bytes = await find_all_copy_pairs(
-                    fs, matches, differs, srconly, dstonly, plan,
-                    srcurl, os.path.join(dst, removeprefix(srcurl, src))
+                # if not informed:
+                #     informed = True
+                #     print(src)
+                child_directory_tasks.append(
+                    asyncio.create_task(find_all_copy_pairs(
+                        fs, matches, differs, srconly, dstonly, plan, srcurl, os.path.join(dst, removeprefix(srcurl, src)), progress, sema
+                    ))
                 )
-                n_files += dir_n_files
-                n_bytes += dir_n_bytes
             else:
                 srconly.write(srcurl + '\n')
                 plan.write(srcurl + '\t' + os.path.join(dst, srcname) + '\n')
                 n_files += 1
                 n_bytes += srcsize
-        else:
-            assert srcname >= dstname
-            dstidx += 1
+            srcidx += 1
+            progress.update(tid, advance=1)
+        while dstidx < len(dstfiles):
+            dstf = dstfiles[dstidx]
+            dstname, dsturl, dstisdir, dstsize = dstf
+
             dstonly.write(dsturl + '\n')
-    while srcidx < len(srcfiles):
-        srcf = srcfiles[srcidx]
-        srcname, srcurl, srcisdir, srcsize = srcf
+            dstidx += 1
+            progress.update(tid, advance=1)
 
-        if srcisdir:
-            if src[-1] != '/':
-                src += '/'
-            if not informed:
-                informed = True
-                print(src)
-            dir_n_files, dir_n_bytes = await find_all_copy_pairs(
-                fs, matches, differs, srconly, dstonly, plan,
-                srcurl, os.path.join(dst, removeprefix(srcurl, src))
-            )
-            n_files += dir_n_files
-            n_bytes += dir_n_bytes
-        else:
-            srconly.write(srcurl + '\n')
-            plan.write(srcurl + '\t' + os.path.join(dst, srcname) + '\n')
-            n_files += 1
-            n_bytes += srcsize
-        srcidx += 1
-    while dstidx < len(dstfiles):
-        dstf = dstfiles[dstidx]
-        dstname, dsturl, dstisdir, dstsize = dstf
+    await asyncio.sleep(0.3)
+    progress.remove_task(tid)
 
-        dstonly.write(dsturl + '\n')
-        dstidx += 1
+    for t in child_directory_tasks:
+        dir_n_files, dir_n_bytes = await t
+        n_files += dir_n_files
+        n_bytes += dir_n_bytes
 
     return n_files, n_bytes
 
 if __name__ == '__main__':
     uvloop_install()
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    with aiomonitor.start_monitor(loop=loop) as monitor:
+        print(f'aiomonitor available on {monitor.host} {monitor.port}')
+        loop.run_until_complete(main())
